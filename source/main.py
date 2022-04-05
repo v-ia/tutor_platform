969353c37481e74a793076f00c7dd627d87c9bbe
Vadim Iatsenko <vadim.v.iatsenko@gmail.com>
2022-04-05 20:19:29 +0300

    feature: add class Bot

--- source/main.py
+++ source/main.py
@@ -1,8 +1,31 @@
 from pathlib import Path
 import logging
 import configparser
-from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+import aiohttp
 
-config = configparser.ConfigParser()
-config.read(Path.cwd().parent / 'config.ini')
-print(config.get('DEFAULT', 'api_token'))
+
+class Bot:
+    def __init__(self, config_filename: str = 'config.ini'):
+        config = configparser.ConfigParser()
+        config.read(Path.cwd().parent / config_filename)
+        self.__bot_token = config.get('DEFAULT', 'bot_token')
+        self.__server_url = config.get('DEFAULT', 'server_url')
+
+    @property
+    async def bot_token(self) -> str:
+        return self.__bot_token
+
+    @property
+    async def server_url(self) -> str:
+        return self.__server_url
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{server_url}bot{bot_token}/getUpdates') as request:
+            json_answer = await request.json()
+            print(json_answer)
+
+if __name__ == '__main__':
+    asyncio.run(main())
