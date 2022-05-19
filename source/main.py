033ccf4f3c524f0e324873839bf793c55dd3a6c2
Vadim Iatsenko <vadim.v.iatsenko@gmail.com>
2022-05-19 20:03:49 +0300

    feature: add global constants

--- source/main.py
+++ source/main.py
@@ -6,8 +6,12 @@ import aiohttp
 import asyncpg
 from aiohttp import web
 
+CONFIG_PATH = Path.cwd().parent
+CONFIG_NAME = 'config.ini'
+SUPPORTED_UPDATE_TYPES = ('message', 'callback_query')
 
-class Database(object):
+
+class Database:
     def __init__(self, config: object):
         self.__connection = None
         self.__auth_data = {'host': config.get('Database', 'host'),
@@ -16,12 +20,13 @@ class Database(object):
                             'password': config.get('Database', 'password'),
                             'database': config.get('Database', 'database')
                             }
-        if None in self.auth_data.values():
-            raise KeyError('Some values for database connection are empty. Problem with reading config file')
 
     @property
     async def auth_data(self) -> dict:
-        return self.__auth_data
+        if None in self.__auth_data:
+            raise KeyError('Some values for database connection are empty. Problem with config file')
+        else:
+            return self.__auth_data
 
     @property
     async def connection(self) -> object:
@@ -49,49 +54,84 @@ class Database(object):
         self.connection.close()
 
 
-
-
-
 class Bot:
-    def __init__(self, config: object):
+    def __init__(self, config: object, database: Database):
         self.__bot_token = config.get('Telegram', 'bot_token')
         self.__server_url = config.get('Telegram', 'server_url')
+        self.__database = database
 
     @property
     async def bot_token(self) -> str:
-        return self.__bot_token
+        if self.__bot_token is None:
+            raise KeyError('Bot_token is empty. Problem with config file')
+        else:
+            return self.__bot_token
 
     @property
     async def server_url(self) -> str:
-        return self.__server_url
+        if self.__server_url is None:
+            raise KeyError('Server_url is empty. Problem with config file')
+        else:
+            return self.__server_url
 
+    @staticmethod
+    async def parser(json_update: dict) -> dict:
+        data = {'update_id': json_update['update_id'],
+                'user_id': json_update[list(json_update)[1]]['from']['id'],
+                'command': json_update[list(json_update)[1]][list(json_update[list(json_update)[1]])[4]]
+                }
+        return data
 
-async def main():
-    config = configparser.ConfigParser()
-    config.read(Path.cwd().parent / 'config.ini')
+    async def request_handler(self, request):
+        json_update = await request.json()
+        update_type = list(json_update)[1]
+        if update_type in SUPPORTED_UPDATE_TYPES:
+            data = await self.parser(json_update)
+            return web.json_response(data)
 
-    bot = Bot(config)
-    print(config.read('Telegram', 'bot_token1'))
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'{await bot.server_url}bot{await bot.bot_token}/getUpdates') as request:
-            json_answer = await request.json()
-            print(json_answer)
-            answer = {'chat_id': json_answer['result'][0]['message']['chat']['id'], 'text': 'Hooray!'}
-            print(answer)
-        # async with session.post(
-        #         f'{await bot.server_url}bot{await bot.bot_token}/sendMessage', json=answer) as request:
-        #     json_answer = await request.json()
-        #     print(json_answer)
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get(f'{await self.server_url}bot{await self.bot_token}/getUpdates') as request:
+        #         json_answer = await request.json()
+        #         print(json_answer)
+        #         sender, text = await self.parser(await request.json())
+        #         answer = {
+        #             'chat_id': sender,
+        #             'text': 'Выберите пункт меню',
+        #             'reply_markup': {
+        #                 'keyboard': [[{'text': 'First button'}], [{'text': 'Second button'}], [{'text': 'Third button'}]]
+        #             }
+        #         }
+        #         answer2 = {
+        #             'chat_id' : sender,
+        #             'text' : 'Выберите пункт меню',
+        #             'reply_markup' : {
+        #                 'remove_keyboard': True
+        #             }
+        #         }
+        #         answer3 = {
+        #             'chat_id' : sender,
+        #             'text' : 'Выберите пункт меню',
+        #             'reply_markup' : {
+        #                 'inline_keyboard' : [[{'text': 'First button', 'callback_data': 1}],
+        #                                      [{'text': 'Second button', 'callback_data': 2}],
+        #                                      [{'text': 'Third button', 'callback_data': 3}]]
+        #             }
+        #         }
+                # async with session.post(
+                #         f'{await self.server_url}bot{await self.bot_token}/sendMessage', json=answer3) as request:
+                #     json_answer = await request.json()
+                #     print(json_answer)
 
-    # row = await conn.fetchrow('SELECT 5*5;')
-    # print(row)
 
 if __name__ == '__main__':
-    asyncio.run(main())
 
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH / CONFIG_NAME)
 
-# async def hello(request):
-#     return web.Response(text="Hello, world")
-# app = web.Application()
-# app.add_routes([web.get('/', hello)])
-# web.run_app(app)
+    postgres_db = Database(config)
+
+    bot = Bot(config, postgres_db)
+
+    app = web.Application()
+    app.add_routes([web.post(f'/', bot.request_handler)])
+    web.run_app(app)
