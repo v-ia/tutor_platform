29c48912040dff17fa7b1ab35cefac892bee4da0
Vadim Iatsenko <vadim.v.iatsenko@gmail.com>
2022-10-25 11:08:33 +0400

    fix: delete unnecessary import

--- source/main.py
+++ source/main.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from database import Database
 from controller import Controller
 from aiohttp import web
-import logging
-from data import *
 import handlers
 
 if __name__ == '__main__':
@@ -17,6 +15,17 @@ if __name__ == '__main__':
     app['controller'].handler_factories = {'/start': handlers.StartFactory,
                                            '/help': handlers.HelpFactory,
                                            '/register': handlers.RegisterFactory,
+                                           '/send_homework': 0,
+                                           '/schedule': 0,
+                                           '/homework': 0,
+                                           '/payments': 0,
+                                           '/cancel_lesson': 0,
+                                           '/reschedule_lesson': 0,
+                                           '/appoint_lesson': 0,
+                                           '/alter_profile': 0,
+                                           '/checked_homework': 0,
+                                           '/lesson_as_paid': 0,
+                                           '/confirm_user': 0
                                            }
     app.add_routes([web.post(f'/', app['controller'].save_update)])
     app.add_routes([web.get(f'/', app['controller'].say_hello)])
