fe2d275256019d622493eecbde071a22c5ca71a6
Vadim Iatsenko <vadim.v.iatsenko@gmail.com>
2022-10-28 20:34:22 +0400

    fix: delete unnecessary route

--- source/main.py
+++ source/main.py
@@ -14,8 +14,7 @@ if __name__ == '__main__':
     app['controller'] = Controller()
     app['controller'].handler_factories = {'/start': handlers.StartFactory,
                                            '/help': handlers.HelpFactory,
-                                           '/register': handlers.RegisterFactory,
+                                           '/register': handlers.RegisterFactory
                                            }
     app.add_routes([web.post(f'/', app['controller'].save_update)])
-    app.add_routes([web.get(f'/', app['controller'].say_hello)])
     web.run_app(app)
