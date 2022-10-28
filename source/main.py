b75cd7d454259eae219142eadddee5ef25aaf6fa
Vadim Iatsenko <vadim.v.iatsenko@gmail.com>
2022-10-28 15:32:16 +0400

    fix: delete unnecessary commands in main

--- source/main.py
+++ source/main.py
@@ -15,17 +15,6 @@ if __name__ == '__main__':
     app['controller'].handler_factories = {'/start': handlers.StartFactory,
                                            '/help': handlers.HelpFactory,
                                            '/register': handlers.RegisterFactory,
-                                           '/send_homework': 0,
-                                           '/schedule': 0,
-                                           '/homework': 0,
-                                           '/payments': 0,
-                                           '/cancel_lesson': 0,
-                                           '/reschedule_lesson': 0,
-                                           '/appoint_lesson': 0,
-                                           '/alter_profile': 0,
-                                           '/checked_homework': 0,
-                                           '/lesson_as_paid': 0,
-                                           '/confirm_user': 0
                                            }
     app.add_routes([web.post(f'/', app['controller'].save_update)])
     app.add_routes([web.get(f'/', app['controller'].say_hello)])
